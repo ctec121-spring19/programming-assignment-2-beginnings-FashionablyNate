cef36a7b09c60c647187ddc73748f0b7a97a6ae1
Nathan J. Spelts <N.SPELTS@labs.clark.edu>
2019-04-17 12:38:02 -0700

    started prob 2

--- Prob-2/Prob-2.py
+++ Prob-2/Prob-2.py
@@ -2,15 +2,15 @@
 #   Programming Assignment 2
 #     Prob-2.py
 
-# YOUR NAME
+# Nathan Spelts
 
 # Follow the steps below. Add your code in the blank line after each comment
 
 # define the main function with no parameters
-
+def main():
     # create a variable and assign it the value returned by an input function
     # that asks the user for a number. Don't forget to use the int() function.
-
+    number = int(input("Enter a number: "))
     # print out a message that says: "The sqare of ? is ?" where the question
     # marks are replaced with the value read in from the user and its square.
 
@@ -23,3 +23,4 @@
     # copy the print statment from above
 
 # call the function main
+main()
